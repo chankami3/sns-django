97b7d5ea1aa165296ae1ad4bed1d7c2fcee41fce
chankami3 <56555940+chankami3@users.noreply.github.com>
2019-10-26 16:25:39 +0900

    Add models script

--- sns/admin.py
+++ sns/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Message,Group,Friend,Good
 
-# Register your models here.
+admin.site.register(Message)
+admin.site.register(Group)
+admin.site.register(Friend)
+admin.site.register(Good)
