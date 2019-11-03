479a62ca62a445f1b2557e5672b91303167624b7
chankami3 <56555940+chankami3@users.noreply.github.com>
2019-11-03 16:12:10 +0900

    Create new application

---
+++ accounts/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render, redirect
+from django.views.generic import FormView
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.contrib.auth import login, authenticate
+
+class UserCreateView(FormView):
+    form_class = UserCreationForm
+    template_name = 'registration/create.html'
+    success_url = reverse_lazy('sns:index')
+
+    def form_valid(self, form):
+        if self.request.POST['next'] == 'confirm':
+            return render(self.request, 'registration/create_confirm.html', {'form':form})
+        elif self.request.POST['next'] == 'regist':
+            form.save()
+            user = authenticate(
+                username = form.cleaned_data['username'],
+                password = form.cleaned_data['password1'],
+            )
+            login(self.request, user)
+            return super().form_valid(form)
+        else:
+            print('error...')
+            return redirect(reverse_lazy('sns:index'))
